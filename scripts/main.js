76128d9c5f16149b018486413fcce8e8ed3b999b
DeltaNedas <deltanedas@gmail.com>
2020-10-03 19:38:47 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,6 @@ anim.addAnimation("test");
 ui.addTable("side", "waffles", t => {
 	const img = t.image(new TextureRegionDrawable(Core.atlas.find("router"))).get();
 	img.size = 64;
+	img.setOrigin(32, 32);
 	anim.animate(img, "test", true);
 });
